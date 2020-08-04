2397cb50fbd0a1fc47c8012341b67c92e34b3881
Bader <abdul.kader.abazli@landdata.de>
2020-08-04 12:36:57 +0200

    add restaurant

--- essensbestellung/src/app/app-routing.module.ts
+++ essensbestellung/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddRestaurantComponent } from './componants/add-restaurant/add-restaurant.component';
+import { RestaurantsListComponent } from './componants/restaurants-list/restaurants-list.component';
 
 
 const routes: Routes = [
-  { path: 'AddRestaurantComponent', component: AddRestaurantComponent }
+  { path: 'addRestaurantComponent', component: AddRestaurantComponent },
+  { path: 'restaurantslistComponent', component: RestaurantsListComponent }
 
 ];
 
