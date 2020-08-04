8bd6397505fa919b082048ac2c10527fb01fab73
Bader <abdul.kader.abazli@landdata.de>
2020-08-04 11:54:47 +0200

    erste virsion

---
+++ essensbestellung/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AddRestaurantComponent } from './componants/add-restaurant/add-restaurant.component';
+import { RestaurantsListComponent } from './componants/restaurants-list/restaurants-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    AddRestaurantComponent,
+    RestaurantsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
