2197d84cbad300599130509fa36b7b1dc7c7bb24
Bader <abdul.kader.abazli@landdata.de>
2020-08-04 12:07:17 +0200

    nav bar

--- essensbestellung/src/app/app-routing.module.ts
+++ essensbestellung/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddRestaurantComponent } from './componants/add-restaurant/add-restaurant.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'AddRestaurantComponent', component: AddRestaurantComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
